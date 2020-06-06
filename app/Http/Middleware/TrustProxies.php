85a4e5ed577591968f879987711acf81b429ee66
Mina <mwfayez@gmail.com>
2020-06-06 22:17:33 +0200

    add aws elb to trusted proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,12 +12,19 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = [
+        '10.192.10.0/24',
+        '10.192.11.0/24'
+    ];
+
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = [
+        Request::HEADER_X_FORWARDED_AWS_ELB,
+        Request::HEADER_X_FORWARDED_PROTO,
+    ];
 }
